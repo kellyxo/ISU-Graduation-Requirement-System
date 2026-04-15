104c28d2fa9f3fc061994c3cdcb3e7c1cb94fe80
Jacob Callicott <jacobc54642@gmail.com>
2026-04-15 08:54:29 -0500

    Update again

--- src/RequirementCatalog.java
+++ src/RequirementCatalog.java
@@ -5,15 +5,15 @@ public class RequirementCatalog {
 
     public static List<Requirement> getRequirements() {
         return Arrays.asList(
-            new Requirement("World Language", Arrays.asList(
+            new Requirement("World Language (3 Credits Remaining)", Arrays.asList(
                 new Course("SPAN101", "Intro to Spanish", 3),
                 new Course("FREN101", "Intro to French", 3)
             )),
-            new Requirement("Math", Arrays.asList(
+            new Requirement("Math (4 Credits Remaining)", Arrays.asList(
                 new Course("MATH201", "Calculus I", 4),
                 new Course("STAT101", "Statistics", 3)
             )),
-            new Requirement("Humanities", Arrays.asList(
+            new Requirement("Humanities (3 Credits Remaining)", Arrays.asList(
                 new Course("HIST101", "World History", 3),
                 new Course("PHIL101", "Intro to Philosophy", 3)
             ))
